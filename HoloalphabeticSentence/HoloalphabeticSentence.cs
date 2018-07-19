ca49b4d10b89d1cfbd008545831c75a1abe7d3b0
Deepika Mohan <deepikamm12@gmail.com>
2018-07-19 12:09:01 +0530

    updated

--- HoloalphabeticSentence/HoloalphabeticSentence.cs
+++ HoloalphabeticSentence/HoloalphabeticSentence.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace HoloalphabeticSentence
 {
@@ -8,7 +10,20 @@ namespace HoloalphabeticSentence
         // Write the logic to determine whether the input string is a HoloalphabeticSentence or not
         public static bool IsHoloalphabeticSentence(string input, string alphabets = "abcdefghijklmnopqrstuvwxyz")
         {
-            return false;
+            char[] inputString =input.ToLower().ToCharArray();
+            if (inputString == null)
+                return false;
+            else
+            {
+                for(int i=0;i<alphabets.Length;i++)
+                {
+                    if(!input.Contains(alphabets[i]))
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
         }
     }
 }
